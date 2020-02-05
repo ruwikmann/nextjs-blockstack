188bd6a10b3fef015cc1b0440fe6f45139f81899
Terje Norderhaug <terje@in-progress.com>
2020-02-04 18:04:31 -0800

    Add blockstack authentication

--- pages/about.js
+++ pages/about.js
@@ -2,19 +2,26 @@ import Link from 'next/link'
 import NavBar from '../components/NavBar'
 import "../styles/index.scss"
 
-const About = () => (
+const About = () => {
+  const { signOut } = useBlockstack()
+  return (
     <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <NavBar />
         <main role="main" className="inner cover">
             <h1 className="cover-heading">This Is The Future of Apps</h1>
             <p className="lead">Next.js + Blockstack is a wicked combo 🤩</p>
             <p className="lead">
-                <Link href='/'><button type="button" className="btn btn-secondary">Sign out</button></Link>
+                <Link href='/'>
+                  <button type="button" className="btn btn-secondary"
+                          disabled={!signOut || null}
+                          onClick={signOut}>
+                    Sign out
+                  </button>
+                </Link>
             </p>
         </main>
-    </div >
-
-
-);
+    </div>
+  )
+};
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 import Link from 'next/link'
 import NavBar from '../components/NavBar';
+import { AppConfig } from 'blockstack'
+import ReactBlockstack, { useBlockstack } from 'react-blockstack'
 import "../styles/index.scss"
 
-const Index = () => (
+const appConfig = new AppConfig(['store_write'])
+const blockstack = ReactBlockstack({appConfig})
+
+const Index = () => {
+  const { signIn } = useBlockstack()
+  return(
     <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <NavBar />
         <main role="main" className="inner cover">
             <h1 className="cover-heading">Decentralized Computing</h1>
             <p className="lead">Welcome to Next.js + Blockstack 🤟</p>
             <p className="lead">
-                <Link href='/about'><button type="button" className="btn btn-secondary">Sign in with Blockstack</button></Link>
+                <Link href='/about'>
+                  <button type="button" className="btn btn-secondary"
+                          disabled={!signIn || null}
+                          onClick={signIn}>
+                    Sign in with Blockstack
+                  </button>
+                </Link>
             </p>
         </main>
     </div>
-);
+  )
+};
 
 export default Index;
